294094e542be7b620573a2c4229ac29f54bf4e5c
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-02-19 15:47:01 -0600

    added the models in index js and fixed category model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
+
 
 module.exports = {
   Product,
@@ -18,3 +30,20 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+User.hasMany(Plan, {
+  foreignKey: 'user_id'
+});
+
+Plan.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Plan.hasMany(Workout, {
+  foreignKey: 'plan_id',
+  onDelete: 'CASCADE'
+});
+
+Workout.belongsTo(Plan, {
+  foreignKey: 'plan_id'
+});
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const sequelize = require('sequelize');
 const routes = require('./routes');
 // import sequelize connection
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+
+sequelize.sync({force:true}).then()
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
